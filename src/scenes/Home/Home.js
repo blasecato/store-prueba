6813c3cbcb887a627b0efdaebb49a8cc47ea1a65
sebastian calderon <bl.calderon@udla.edu.co>
2020-07-16 12:03:23 -0500

    feat (intall react router dom)

--- src/scenes/Home/Home.js
+++ src/scenes/Home/Home.js
@@ -1,24 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Icon, Input, Button, Spin, notification, Alert, Checkbox } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import { useDispatch } from 'react-redux';
 import { Link } from "react-router-dom";
-import { auth } from '../../../services/Auth/AuthActions';
 
 
 export const Home = () => {
 
-	const dispatch = useDispatch()
 
-	const handleLogout = () => {
-		dispatch(auth.logout())
-	}
+
 
 	return (
 		<div className="SingUp">
-			hola home iniciaste sesion
+			hola 
 
-			<Button className="button" onClick={()=> handleLogout()}>Cerrar sesión</Button>
+			<Button className="button">Cerrar sesión</Button>
 
 		</div>
 	);
